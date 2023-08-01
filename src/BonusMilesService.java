dc57366f5be17d6ed3479c754f87866be1b6e783
Julia Nemkina <nemkina.julia@mail.ru>
2023-08-01 20:17:27 +0300

    connection

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate(int price) {
+        int bonus = 20;
+        int miles;
+        if (price >= 20) {
+            miles = price / bonus;
+        } else {
+            miles = 0;
+        }
+        return miles;
+    }
 }
